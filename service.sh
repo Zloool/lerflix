439f0b2d7cee28691d142c8208eae16c0de7fbde
Zloool <zhavoronkov.p@gmail.com>
2022-05-14 15:47:04 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/zlol/leroos/lerflix
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 /home/zlol/leroos/lerflix/main.py # >> /code/logs/myapp.log 2>&1
